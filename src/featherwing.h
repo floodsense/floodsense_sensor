025d3d7f4110b200083b4c6a47d39bdb55b50949
jatinpalchuri <72412852+jatinpalchuri@users.noreply.github.com>
2021-07-07 17:24:12 -0400

    Update featherwing.h

--- src/featherwing.h
+++ src/featherwing.h
@@ -1,3 +1,4 @@
+
 #ifndef FEATHERWING_H
 #define FEATHERWING_H
 
@@ -5,7 +6,8 @@
 #include <SdFat.h>
 
 void writeToSDCard(String StringtobeWritten);
-
+uint8_t readkey(String filename);
+void writekey(String filename);
 //RTC
 #include "RTClib.h"
 
